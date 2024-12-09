3c659311978e2dcb83b96b3231eabb2f298eb377
MrTulok <tulokvendel@gmail.com>
2024-12-09 21:25:49 +0100

    masodik

--- WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61048203e4e8db11bcf7172ae25e2873149aa521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa9139b48594de55d2e60783ce3d7e210d8d11a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
